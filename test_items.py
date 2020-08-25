4afb7ff1a03f850a064a0324fc72b75369bcb1ef
Sergjey <Strongestiq@gmail.com>
2020-08-25 13:25:26 +0300

    analogue of assert added

--- test_items.py
+++ test_items.py
@@ -8,6 +8,7 @@ def test_button_in_different_languages(browser):
     # you can try to use: pytest --language=es test_items.py
     # or: pytest -s -v --language=fr test_items.py
     button = browser.find_element_by_css_selector("[class='btn btn-lg btn-primary btn-add-to-basket']")
+    #or assert True
     assert button, "Oops, we can't find button"
     #убеждаемся, что кнопку действительно нашли. без ручной проверки time.sleep()
     print('\n' + button.text)
